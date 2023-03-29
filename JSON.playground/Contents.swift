bedc211bc7a6fe6973e3614c6043cc6d84ac99b4
Vitaliy <vmolokov59@gmail.com>
2023-03-29 18:02:04 +0300

    Add structs

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+struct Card: Codable {
+    let name: String
+    let type: String
+    let manaCost: String?
+    let setName: String
+    let rarity: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case type
+        case manaCost = "manaCost"
+        case setName = "set"
+        case rarity
+    }
+}
+
+struct CardsResponse: Codable {
+    let cards: [Card]
+}
